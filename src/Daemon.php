7fee46b8d79e57194727375a43d01dafa2ed3ebe
Jason Favrod <jason@lakonacomputers.com>
2016-06-09 03:03:19 -0400

    Added Count Parameter to playlistVideos Method
    Also added documentation.

--- src/Daemon.php
+++ src/Daemon.php
@@ -27,12 +27,35 @@ class Daemon
     }
 
 
-    public static function playlistVideos($playlistId)
+    /**
+     * playlistVideos
+     * 
+     * @param string $playlistId The id of the YouTube playlist.
+     * @param int $count (Optional) The number of items to grab; all
+     * if not given.
+     *
+     * @return array An array containing YouTubeVideo Objects
+     * representing the items in the YouTube playlist; empty on fail.
+     */
+
+    public static function playlistVideos($playlistId, $count=0)
     {
         $videos = [];
+        $totalVideos = 0;
 
-        $query    = self::$url.'/playlistItems?part=snippet&playlistId=' .
-            $playlistId.'&fields=items&key='.self::$key;
+        $query = self::$url.'/playlistItems?part=snippet&playlistId=' .
+            $playlistId.'&key='.self::$key;
+
+        if ($count > 0) {
+            $totalVideos =
+                json_decode(file_get_contents($query))->pageInfo->totalResults;
+
+            if ($count <= $totalVideos) {
+                $query .= '&maxResults='.$count;
+            }
+        }
+
+        $query .= '&fields=items';
         
         foreach (json_decode(file_get_contents($query))->items as $playlistItem) {
             array_push($videos, new Video($playlistItem->snippet));
